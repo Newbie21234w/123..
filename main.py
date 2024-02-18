e4b051edc4477263aa4e0e261eac0778db475570
smile <smilemarket89@gmail.com>
2024-02-18 12:33:37 -0800

    https://github.com/Newbie21234w/123..

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+url = "https://cash-backer.club/shops"
+while True:
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        all_products_rate = soup.find_all("div", class_="shop-rate")
+        for rate in all_products_rate:
+            print("Rate:", rate.text.strip())
+        all_products_title = soup.find_all("div", class_="shop-title")
+        for title in all_products_title:
+            print("Tile:", title.text.strip())
+        pagination = soup.find("a", class_="page-link")
+        if not pagination:
+            break
